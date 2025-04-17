daa2ef924d8e7828933009190a6c2afa01ae72e2
niclaslueth <niclas.lueth@gmail.com>
2025-04-17 21:36:04 +0200

    automode for codespaces /coder

--- install.sh
+++ install.sh
@@ -1,79 +1,103 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+set -e
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
-git pull origin master;
+git pull origin master
+
+# *Automodus erkennen:*
+# Für Codespaces: CODESPACES=true
+# Für Coder Workspaces: meistens ist CODER_ENV oder CODER_WORKSPACE_ID gesetzt
+AUTO_MODE=false
+if [[ "$CODESPACES" == "true" || -n "$CODER_ENV" || -n "$CODER_WORKSPACE_ID" ]]; then
+    AUTO_MODE=true
+fi
+# Der Parameter --auto aktiviert AUTOMODUS zusätzlich (zB. für Tests/Lokal)
+if [[ "$1" == "--auto" ]]; then
+    AUTO_MODE=true
+fi
+
+# "ask"-Funktion, automatische Beantwortung im AUTO_MODE
+ask() {
+    # $1=Fragetext, $2=Default-Antwort (y/n)
+    if [ "$AUTO_MODE" = true ]; then
+        REPLY="$2"
+        echo "$1 ($2)   [automatisch: $2]"
+    else
+        read -p "$1 ($2) " -n 1
+        echo ""
+    fi
+}
 
 function doIt() {
-
-    read -p "Are you on MAC? (y/n) " -n 1;
-    echo "";
+    # MAC: immer NEIN im Auto-Mode (z.B. Codespaces/Coder laufen nie auf Mac)
+    ask "Are you on MAC?" "n"
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        export IS_MAC=true;
+        export IS_MAC=true
     else
-        export IS_MAC=false;
-    fi;
-    
-    source ./_install/brew.sh;
+        export IS_MAC=false
+    fi
+
+    source ./_install/brew.sh
 
     if [[ "$IS_MAC" == true ]]; then
-        read -p "Would you like to put on my Mac Dock (y/n) " -n 1;
-        echo "";
+        ask "Would you like to put on my Mac Dock" "n"
         if [[ $REPLY =~ ^[Yy]$ ]]; then
-            source ./macOS/dock.sh;
-        fi;
-    fi;
+            source ./macOS/dock.sh
+        fi
+    fi
 
-    read -p "Do you want to use my zsh and oh-my-zsh settings? (y/n) " -n 1;
+    ask "Do you want to use my zsh and oh-my-zsh settings?" "y"
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         source ./_install/zsh.sh
-    fi;
+    fi
 
-    read -p "Do you want to use asdf? (y/n) " -n 1;
+    ask "Do you want to use asdf?" "n"
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        source ./asdf/plugins.sh;
-    fi;
+        source ./asdf/plugins.sh
+    fi
 
     if [[ "$IS_MAC" == true ]]; then
-        read -p "Would you like to use Mackup? (Keep your application settings in sync (OS X/Linux). https://github.com/lra/mackup) (y/n)" -n 1;
+        ask "Would you like to use Mackup? (Keep your application settings in sync) (OS X/Linux)" "n"
         if [[ $REPLY =~ ^[Yy]$ ]]; then
             source ./_install/mackup.sh
-        fi;
-    fi;
+        fi
+    fi
 
-    read -p "Do you want to use Vim and NeoVim? (y/n) " -n 1;
+    ask "Do you want to use Vim and NeoVim?" "y"
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         source ./_install/nvim.sh
-    fi;
+    fi
 
-    read -p "Do you want to have my Development/Project folder structure? (y/n) " -n 1;
+    ask "Do you want to have my Development/Project folder structure?" "n"
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         source ./macOS/project-folder-structure.sh
-    fi;
+    fi
 
     source ./_install/git.sh
 
     if [[ "$IS_MAC" == true ]]; then
-        source ./macOS/settings.sh;
-    fi;
+        source ./macOS/settings.sh
+    fi
 
     echo ""
     echo "Set your user tokens as environment variables, such as ~/.secrets"
     echo "See the README for examples."
 
-    read -p "Do you want to restart now (This is recommended now)? (y/n) " -n 1;
+    ask "Do you want to restart now? (Recommended)" "n"
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         sudo reboot
-    fi;
+    fi
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    doIt;
+# Wenn Skript mit --force, -f oder --auto aufgerufen wird, dann los.
+if [[ "$1" == "--force" || "$1" == "-f" || "$1" == "--auto" ]]; then
+    doIt
 else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-    echo "";
+    ask "This may overwrite existing files in your home directory. Are you sure?" "n"
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        doIt;
-    fi;
-fi;
-unset doIt;
+        doIt
+    fi
+fi
+
+unset doIt
